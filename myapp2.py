41c56cdccb374e6d66d7c1b20a80981534d4a100
amebarek87 <87528794+amebarek87@users.noreply.github.com>
2022-03-28 23:18:58 +0200

    Update myapp2.py

--- myapp2.py
+++ myapp2.py
@@ -1,33 +1,7 @@
 import streamlit as st
-import streamlit.components.v1 as stc
 
 # File Processing Pkgs
 import pandas as pd
 
 
-
-
-
-# import fitz  # this is pymupdf
-
-# def read_pdf_with_fitz(file):
-# 	with fitz.open(file) as doc:
-# 		text = ""
-# 		for page in doc:
-# 			text += page.getText()
-# 		return text
-
-# Fxn
-
-
-
-def main():
-    st.title("TABLERO")
-
-    menu =["Datos"]
-    choice = st.sidebar.selectbox("Menu", menu)
-   
-
-
-if __name__ == '__main__':
-    main()
+st.write("hello to all")
